838b7a47857a5a7cf494cc51f0b9a9e79b8abbc0
jeremias24 <jeremias-bartolome@isuzuphil.com>
2025-06-13 15:12:32 +0800

    google map implementation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="card">
     <div class="card-header">
-      <h3 class="card-title">About Us</h3>
+      <h3 class="card-title">Location</h3>
     </div>
     <div class="card-body">
       <p>
-        Welcome to our dashboard! Here you can find key information about our
-        project, team, and mission.
+      
       </p>
+
+      <!-- Responsive Google Map Embed -->
+      <div class="map-container" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; margin-top: 1rem;">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3867.639557800745!2d121.15905777456645!3d14.215871086381195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd63005843a003%3A0xcea058307c5842cf!2sSphnix%20Welding%20Shop!5e0!3m2!1sen!2sph!4v1749797697459!5m2!1sen!2sph"
+          style="border:0; position: absolute; top:0; left: 0; width: 100%; height: 100%;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+
       <!-- add more about-content here -->
     </div>
   </div>
@@ -17,6 +28,6 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "about",
+  name: "About",
 });
 </script>
