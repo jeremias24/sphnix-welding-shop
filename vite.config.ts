b98b7ecf24656ba1f3ab055730a96a25d5cc84ca
jeremias24 <jeremias-bartolome@isuzuphil.com>
2025-06-19 19:22:10 +0800

    pwa installation

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,34 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Sphnix Welding Shop',
+        short_name: 'Sphnix',
+        description: 'A welding shop product and service platform',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa/phoenix-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
