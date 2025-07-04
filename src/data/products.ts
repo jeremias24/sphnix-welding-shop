5d4c8b45dfb65eaed8b99ad2d7e6507785e2edf3
jeremias24 <jeremias-bartolome@isuzuphil.com>
2025-07-04 08:18:51 +0800

    products update

--- src/data/products.ts
+++ src/data/products.ts
@@ -13,7 +13,7 @@ export const products = [
     price: '27,000',
     category: 'Vehicle Add-ons',
     path: 'products/sidecars/1',
-    image: ['sidecar1.jpg', 'sidecar2.jpg', 'sidecar3.jpg'] // use 3 different images
+    image: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'] // use 3 different images
   },
   {
     id: 3,
@@ -22,5 +22,13 @@ export const products = [
     category: 'Vehicle Add-ons',
     path: 'products/e_bikes/1',
     image: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'],  // use 3 different images
+  },
+  {
+    id: 4,
+    name: 'Bench Press Machine',
+    price: '7,000',
+    category: 'Fitness',
+    path: 'products/gym_equipments/2',
+    image: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'],  // use 3 different images
   },
 ]
